aea9b7afa130ee936747b686463a43e8bf82b3f1
prince <mrepte700@gmail.com>
2021-07-06 19:28:35 +0600

    get a specific products with query parameters

--- index.js
+++ index.js
@@ -19,10 +19,33 @@ const products = [
 ]
 
 //show list of products
-app.get('/products',(req, res) => {
+app.get('/api/products',(req, res) => {
     res.json(products);
 });
 
+//show a specific products
+app.get('/api/products/:id', (req, res) => {
+    const {id} = req.params;
+    const products = products.find(prod => prod.id === id);
+
+    if(!products){
+        return res.status(404).json({
+            error: 'No Products Found With this ID'
+        })
+    }
+    
+    return res.json(products);
+})
+
+//insert a product
+
+//update a specific product(PUT)
+
+//update a specific product(PATCH)
+
+//Delete a specific product
+
+//Delete all products
 
 
 app.listen(3000, () => console.log('Server is running at port 3000'));
